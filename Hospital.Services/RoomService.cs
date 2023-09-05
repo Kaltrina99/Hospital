a696900d4955fd82355bcadc1586de5243273ec7
Aoiko <36515858+Kaltrina99@users.noreply.github.com>
2023-09-05 11:44:26 +0200

    RoomService

---
+++ Hospital.Services/RoomService.cs
@@ -0,0 +1,89 @@
+﻿using Hospital.Modals;
+using Hospital.Repositories.Interfaces;
+using Hospital.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.Services
+{
+    public class RoomService :IRoomService
+    {
+        private IUnitOfWork _unitOfWork { get; set; }
+        public RoomService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public PagedResult<RoomViewModel> GetAll(int pageNumber, int pageSize)
+        {
+            var RoomViewModel = new RoomViewModel();
+            int totalCount;
+            List<RoomViewModel> vmList = new List<RoomViewModel>();
+            try
+            {
+                int ExcludeRecords = (pageSize - pageNumber) - pageSize;
+                var modelList = _unitOfWork.GenericRepository<Room>().GetAll()
+                    .Skip(ExcludeRecords).Take(pageSize).ToList();
+                totalCount = _unitOfWork.GenericRepository<Room>().GetAll().ToList().Count;
+                vmList = ConvertModeltoViewModelList(modelList);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            var result = new PagedResult<RoomViewModel>
+            {
+                Data = vmList,
+                TotalItems = totalCount,
+                PageNumber = pageNumber,
+                PageSize = pageSize
+            };
+            return result;
+        }
+
+        private List<RoomViewModel> ConvertModeltoViewModelList(List<Room> modelList)
+        {
+            return modelList.Select(x => new RoomViewModel(x)).ToList();
+        }
+
+        public RoomViewModel GetRoomById(int RoomId)
+        {
+            var model = _unitOfWork.GenericRepository<Room>().GetById(RoomId);
+            var vm = new RoomViewModel(model);
+            return vm;
+        }
+
+        public void UpdateRoom(RoomViewModel Room)
+        {
+            var model = new RoomViewModel().ConvertViewModel(Room);
+            var ModelById = _unitOfWork.GenericRepository<Room>().GetById(model.Id);
+            ModelById.Id = model.Id;
+            ModelById.RoomNumber = Room.RoomNumber;
+            ModelById.Type= Room.Type;
+            ModelById.Status = Room.Status;
+            ModelById.HospitalId=ModelById.HospitalId;
+            _unitOfWork.GenericRepository<Room>().Update(ModelById);
+            _unitOfWork.Save();
+        }
+
+        public void InsertRoom(RoomViewModel Room)
+        {
+            var model = new RoomViewModel().ConvertViewModel(Room);
+            _unitOfWork.GenericRepository<Room>().Add(model);
+            _unitOfWork.Save();
+        }
+
+        public void DeleteRoom(int id)
+        {
+            var model = _unitOfWork.GenericRepository<Room>().GetById(id);
+            _unitOfWork.GenericRepository<Room>().Delete(model);
+            _unitOfWork.Save();
+        }
+        
+    }
+
+}
---
+++ Hospital.ViewModels/RoomViewModel.cs
@@ -0,0 +1,40 @@
+﻿using Hospital.Modals;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital.ViewModels
+{
+    public class RoomViewModel
+    {
+        public int Id { get; set; }
+        public string RoomNumber { get; set; }
+        public string Type { get; set; }
+        public string Status { get; set; }
+        public int HospitalId { get; set; }
+        public RoomViewModel() { }
+        public RoomViewModel(Room model)
+        {
+            Id = model.Id;
+            RoomNumber= model.RoomNumber;
+            Type = model.Type;
+            Status = model.Status;  
+            HospitalId = model.HospitalId;
+
+        }
+        public Room ConvertViewModel(RoomViewModel model)
+        {
+
+            return new Room
+            {
+                Id = model.Id,
+                RoomNumber = model.RoomNumber,
+                Type = model.Type,
+                Status = model.Status,
+                HospitalId = model.HospitalId
+             };
+        }
+    }
+}
